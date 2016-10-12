4d7e6ebe6ba666c32050a55c0b4975976abb9444
Tetsuji-Yabuki <t.yabuki@gloops.com>
2016-10-12 17:08:35 +0900

    first commit

---
+++ SmartBot/Controllers/MessagesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace SmartBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (ActivityTypes.Message.Equals(activity.Type))
+            {
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                var reply = activity.CreateReply($"You sent [{activity.Text}]");
+
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            switch (message.Type)
+            {
+                case ActivityTypes.DeleteUserData:
+                case ActivityTypes.ConversationUpdate:
+                case ActivityTypes.ContactRelationUpdate:
+                case ActivityTypes.Typing:
+                case ActivityTypes.Ping:
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
